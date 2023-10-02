f37b98648d92b98355561e39689f665046f4d22c
aninhasss <102299765+aninhasss@users.noreply.github.com>
2023-10-02 15:16:51 -0300

    Update ex10.php

--- ex10.php
+++ ex10.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="molde.css" type="text/css">
-    <title>questão-1</title>
+    <title>questão-10</title>
 </head>
 
 <body>
